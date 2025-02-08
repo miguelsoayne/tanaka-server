e7d26258d16d808c837f656a89216ce034e1e8a2
Miguel Soayne Magaño <legacykain777779@hotmail.com>
2025-02-08 04:32:21 -0500

    add lombok dependency

--- src/main/java/edu/galaxy/tanaka/entities/Product.java
+++ src/main/java/edu/galaxy/tanaka/entities/Product.java
@@ -5,9 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "Product")
 @Table(name = "tbl_products")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 	
 	@Id
@@ -19,62 +27,4 @@ public class Product {
 	private Double precio;
 	private String image;
 	
-	public Product() {
-	}
-
-	public Product(String name, String description, int stock, Double precio, String image) {
-		this.name = name;
-		this.description = description;
-		this.stock = stock;
-		this.precio = precio;
-		this.image = image;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getStock() {
-		return stock;
-	}
-
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-
-	public Double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(Double precio) {
-		this.precio = precio;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
 }
