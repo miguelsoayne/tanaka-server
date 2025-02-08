5a4e618324a6238fc5baee70c11dc0eae0a724d4
Miguel Soayne Magaño <legacykain777779@hotmail.com>
2025-02-08 04:09:06 -0500

    clean main file

--- src/main/java/edu/galaxy/tanaka/TanakaApplication.java
+++ src/main/java/edu/galaxy/tanaka/TanakaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("edu.galaxy.tanaka.repositories")
 public class TanakaApplication {
 
 	public static void main(String[] args) {
