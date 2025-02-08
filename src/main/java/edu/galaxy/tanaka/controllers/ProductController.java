442f79ca6be04e60e0e812173d5af961b13a6afd
Miguel Soayne Magaño <legacykain777779@hotmail.com>
2025-02-08 05:10:58 -0500

    add POST / and GET /{id}

--- src/main/java/edu/galaxy/tanaka/controllers/ProductController.java
+++ src/main/java/edu/galaxy/tanaka/controllers/ProductController.java
@@ -1,9 +1,13 @@
 package edu.galaxy.tanaka.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +26,14 @@ public class ProductController {
 		return productRepository.findAll();
 	}
 
+	// /api/v1/products/1
+	@GetMapping("/{id}")
+	public Optional<Product> findById(@PathVariable Long id) {
+		return productRepository.findById(id);
+	}
+	
+	@PostMapping
+	public Product save(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
 }
